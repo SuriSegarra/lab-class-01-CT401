ae037a0b72bad2061eceb138b89d267d6c825780
surelis segarra <sdarianne34@gmail.com>
2020-03-13 08:41:42 -0700

    i think I almost done

--- Model.test.js
+++ Model.test.js
@@ -60,17 +60,54 @@ describe('Model class', () => {
       })
       .then(cat => {
         return Cat
-          .findByIdAndUpdate(cat._id, { name: 'rover' });
+          .findByIdAndUpdate(cat._id, { name: 'Tom' });
       })
       .then(updatedCat => {
         expect(updatedCat).toEqual({
           _id: expect.any(String),
-          name: 'Tim',
+          name: 'Tom',
           age: 4,
           weight: '10 lbs'
         });
       });
   });
+
+  it('find by id', () => {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number,
+        required: true
+      },
+      weight: {
+        type: String,
+        required: true
+      }
+    });
   
+    const Cat = new Model('Cat', schema);
+
+    return Cat
+      .create({
+        name: 'Tom',
+        age: 4,
+        weight: '10 lbs'
+      })
+      .then(cat => {
+        return Cat
+          .findById(cat._id);
+      })
+      .then(updateCat => {
+        expect(updateCat).toEqual({
+          _id: expect.any(String),
+          name: 'Tom',
+          age: 4,
+          weight: '10 lbs'
+        });
+      });
+  });
 });
 
